915f0873233a833942b0bea4d9292a99f2127bf3
Stian Jensen <me@stianj.com>
2018-10-13 22:43:26 +0200

    Open eyes instead of pupils

--- src/tvboy.js
+++ src/tvboy.js
@@ -38,27 +38,44 @@
       this.ctx.scale(16 * GU / 1920, 16 * GU /  1920);
       this.ctx.drawImage(this.imageElement, 0, 0);
 
-      const doubleColonialWhite = '#eedda5';
-      this.ctx.fillStyle = doubleColonialWhite;
+      const grey = '#909090';
+      this.ctx.fillStyle = grey;
+
+      const open = easeIn(0, 1, (this.frame - 3860) / 25);
+      const bottomOpen = lerp(45, 0, open * 2);
+      const topOpen = lerp(1, 0, open * 2 - 1);
 
       this.ctx.beginPath();
-      this.ctx.ellipse(862,
-                       338,
-                       45,
-                       35,
-                       0, 0, Math.PI * 2);
+      this.ctx.ellipse(868,
+                       288 + 45 * topOpen,
+                       55 * topOpen,
+                       45 * topOpen,
+                       0, Math.PI, Math.PI * 2);
+      this.ctx.fill();
+      this.ctx.beginPath();
+      this.ctx.ellipse(868,
+                       332,
+                       55,
+                       bottomOpen,
+                       0, 0, Math.PI);
       this.ctx.fill();
 
       this.ctx.beginPath();
       this.ctx.ellipse(1089,
+                       288 + 45 * topOpen,
+                       55 * topOpen,
+                       45 * topOpen,
+                       0, Math.PI, Math.PI * 2);
+      this.ctx.fill();
+      this.ctx.beginPath();
+      this.ctx.ellipse(1089,
                        332,
-                       45,
-                       35,
-                       0, 0, Math.PI * 2);
+                       55,
+                       bottomOpen,
+                       0, 0, Math.PI);
       this.ctx.fill();
 
-      const open = easeIn(0, 35, (this.frame - 3860) / 25);
-
+      /*
       this.ctx.drawImage(
         this.imageElement,
         862 - 45/2, 338 - 35/2,
@@ -74,6 +91,7 @@
         1089 - 45/2, 332 - open/2,
         45, open
       );
+      */
 
       this.ctx.restore();
     }
